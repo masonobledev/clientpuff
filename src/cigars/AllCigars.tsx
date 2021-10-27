4cb99e247d5bf20a7bc7573d54c8424867645acc
Patrick Cook <masonobledev@gmail.com>
2021-10-26 23:41:55 -0400

    11:41pm

--- src/cigars/AllCigars.tsx
+++ src/cigars/AllCigars.tsx
@@ -1,53 +1,79 @@
-import React, { Component } from 'react';
-import { Card } from 'antd';
-import APIURL from '../helpers/environment';
+import React, { Component } from "react";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardHeader,
+} from "@material-ui/core/";
+import APIURL from "../helpers/environment";
 
 type AllProps = {
-	token: string;
+  token: string;
 };
 
 export default class AllCigars extends Component<AllProps, any> {
-    constructor(props: AllProps) {
-        super(props);
-        this.state = {
-            cigars: []
-        };
-    }
-
-    componentDidMount() {
-
-        fetch(`${APIURL}/cigar/`, {
-            method: 'GET',
-            headers: ({
-                Authorization: this.props.token
-            })
-        })
-        .then((response) => response.json())
-        .then(cigarsList => {
-            this.setState({cigars: cigarsList.data});
-            console.log('this.state.cigars');
-            console.log(this.state.cigars);
-        });
+  constructor(props: AllProps) {
+    super(props);
+    this.state = {
+      cigars: [],
     };
+  }
 
-    render () {
-        return (
-            <div>
-             
-            {this.state.cigars.map((cigar: any) =>
-               <Card  title={cigar.brand} size = 'default' bordered = {true} style={{ width: 400 }}>
-                <p><b>Origin:</b> {cigar.origin}</p>
-                <p><b>Profile:</b> {cigar.profile}</p>
-                <br />
+  componentDidMount() {
+    fetch(`${APIURL}/cigar/`, {
+      method: "GET",
+      headers: {
+        Authorization: this.props.token,
+      },
+    })
+      .then((response) => response.json())
+      .then((cigarsList) => {
+        this.setState({ cigars: cigarsList.data });
+        console.log("this.state.cigars");
+        console.log(this.state.cigars);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          {this.state.cigars.map((cigar: any) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={3}
+              key={this.state.cigars.indexOf(cigar)}
+            >
+              <Card>
+                <CardHeader
+                  title={`${cigar.brand}`}
+                  subheader={`origin : ${cigar.origin}`}
+                />
+                <CardContent>
+                  <p>
+                    <b>Profile:</b> {cigar.profile}
+                  </p>
+                  <p>
+                    <b>Shape:</b> {cigar.shape}
+                  </p>
+                  <p>
+                    <b>Wrapper:</b> {cigar.wrapper}
+                  </p>
+                </CardContent>
               </Card>
-            )}
-            </div>
-        );
-    }
-};
-    
-    
-    
-
-
-
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    );
+  }
+}
